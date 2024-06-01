8cf9f4a326434d51d3c7c18a5cc84737ef3788a6
mhoarb <mho.arby@gmail.com>
2024-06-02 01:40:48 +0330

    complete choose question function

--- internal/play/chooseQuestion.go
+++ internal/play/chooseQuestion.go
@@ -28,22 +28,30 @@ func ChooseQuestion() {
 	if err := db.First(&flashCard, questionID).Error; err != nil {
 		slog.Error("Failed to retrieve question:", err)
 	}
-	slog.Info("please write the Answer of this question")
-	answer, _ := reader.ReadString('\n')
-	answer = answer[:len(answer)-1]
 
-	if answer == flashCard.Answer {
-		flashCard.Status = "correct"
-		slog.Info("your answer is correct")
-		if err := db.Save(&flashCard).Error; err != nil {
-			log.Fatalf("Error updating question: %v", err)
-		}
-	} else {
-		flashCard.Status = "Not correct"
-		slog.Info("your answer isn''t correct, please try again`")
-		if err := db.Save(&flashCard).Error; err != nil {
-			log.Fatalf("Error updating question: %v", err)
-		}
+	if flashCard.Status == "correct" {
+		slog.Error("this Question is already correct and you answer this")
+		slog.Info("please choose another question")
 		Play()
+	} else {
+		slog.Info("please write the Answer of this question")
+		answer, _ := reader.ReadString('\n')
+		answer = answer[:len(answer)-1]
+
+		if answer == flashCard.Answer {
+			flashCard.Status = "correct"
+			slog.Info("your answer is correct")
+			if err := db.Save(&flashCard).Error; err != nil {
+				log.Fatalf("Error updating question: %v", err)
+			}
+			Play()
+		} else {
+			flashCard.Status = "Not correct"
+			slog.Info("your answer isn''t correct, please try again`")
+			if err := db.Save(&flashCard).Error; err != nil {
+				log.Fatalf("Error updating question: %v", err)
+			}
+			Play()
+		}
 	}
 }
--- internal/play/playFlashcards.go
+++ internal/play/playFlashcards.go
@@ -1,15 +1,16 @@
 package play
 
 import (
+	db2 "flashCardProject/db"
 	"flashCardProject/internal"
 	"flashCardProject/internal/play/displayTable"
 	"log"
 )
 
 func Play() {
-	db := SetCardStatus()
+	db, err := db2.SetUpDatabase()
 	var flashCards []internal.FlashCard
-	flashCards, err := internal.GetAllFlashcardsData(db)
+	flashCards, err = internal.GetAllFlashcardsData(db)
 	if err != nil {
 		log.Fatalln(err)
 	}
